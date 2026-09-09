b47dbc8fdc50d132499292cc2104a72290f05e21
Aaron Robinson <bobafettsjetpack@gmail.com>
2026-09-09 11:06:46 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name: Aaron Robinson
+* Course: CSCI 1250, Section 001
+* Assignment: Lab 0, Hello World
+* Date: September 9, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Aaron Robinson.");
+Console.WriteLine("This is my first program at ETSU.");
